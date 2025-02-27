438f529b2c8feb6eaa65eb9727fd5c05b7c9ae47
sudhakarsingajogi <ssr.sudhakar@gmail.com>
2025-02-27 14:28:55 +0530

    added HashRouter instead of browserroter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "../src/assets/css/font-awesome-all.css";
 import "../src/assets/css/flaticon.css"; 
